5c8dd91d2c69feef02c3300c5043ccceb6066817
Sebastien <sebgayepro@gmail.com>
2020-04-22 12:06:47 +0200

    commentaire

---
+++ traducteur.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Traducteur</title>
+</head>
+<body>
+    <h1 style="color: grey">Traducteur Anglais - Français</h1>
+    <label for="mot">Mot:</label>
+    <input type="text" id="mot" name="mot">
+    <br><br>
+    <label for="traduction">Sens de traduction:</label>
+    <select name="traduction">
+      <option value="first">Anglais-Français</option>
+      <option value="second">Français-Anglais</option>
+    </select>
+
+    <br><br>
+    <input type="button" value="Traduire">
+</body>
+</html>
